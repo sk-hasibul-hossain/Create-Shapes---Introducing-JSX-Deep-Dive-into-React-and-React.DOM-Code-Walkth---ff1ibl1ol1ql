3c4eaeaae00ccc490b8e51669ce6429afc9f32ed
SK Hasibul Hossain <86801974+sk-hasibul-hossain@users.noreply.github.com>
2023-08-23 01:41:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import '../styles/App.css';
 const App = () => {
-
+  const [shape,setShape] = useState([]);
+  let newShape = shape.length === 0 ? "square" : shape[shape.length-1];
+  const onOptionChangeHandler = (event) => {
+    newShape = event.target.value;
+  }
+  const addShape = () => {
+    setShape([...shape,newShape]);
+  }
   return (
     <div id="main">
       <div id="shape-creator">
-        
-
+        <select onChange={onOptionChangeHandler}>
+          <option value="square">Square</option>
+          <option value="circle">Circle</option>
+        </select>
+        <button onClick={addShape}>Add Shape</button>
       </div>
       <div id="shapes-holder">
+        {shape.map((s,index) => {
+          return <div key={index} className={s}>{index}</div>;
+        })}
       </div>
     </div>
   )
